244c25b278ad7314ab5fdcad6c9e67da109ef67e
Christian Wörner <christianworner@gmail.com>
2011-01-28 01:15:43 +0100

    OSC_hub: add logging for send-exceptions
    * run server that listens on the specified UDP port to avoid exceptions

--- OSC_hub.py
+++ OSC_hub.py
@@ -1,18 +1,25 @@
 import logging
 from OSC import OSCClient, OSCMessage, OSCClientError
 
+HOST = "localhost"
+PORT = 9999
 
 logger = logging.getLogger("OSC_hub")
 client = OSCClient()
-client.connect( ('localhost', 9999))
+client.connect((HOST, PORT))
 
-def send(address, msg):        
+
+def send(address, msg):
     m = OSCMessage(address)
     m.message = msg
     try:
         client.send(m, 20)
     except OSCClientError:
+        logger.error("error sending OSC message: {0} to {1}:{2}".format(msg,
+                                                                        HOST,
+                                                                        PORT))
         pass
+
 
 def hub():
     while True:
@@ -23,13 +30,11 @@ def hub():
                 if v.note_change:
                     address = str(v.id)
                     msg = str(v.note)
-                    logger.info("sending out: /{0}/{1} for id:{2}".format(address, msg, v.id))
+                    logger.info("sending out: /{0}/{1} for id:{2}".\
+                               format(address, msg, v.id))
                     send(address, msg)
             #address = data["voice"]
             #msg = data["message"]
         else:
             address = "gen"
             msg = str(data)
-        
-        
-
