acd7b4b8a2577a868299d432fed6d0e055604436
Christian Wörner <christianworner@gmail.com>
2011-02-13 18:51:00 +0100

    Composer: improve harmony detection
    * according to voice number

--- composer.py
+++ composer.py
@@ -14,7 +14,12 @@ MELODIC = [1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1]
 
 STRICT_HARMONIES = [set([2, 4, 6]), set([2, 4, 0]),
                     set([3, 5, 0]), set([2, 5, 0])]
-BASE_HARMONIES = [set([2 , 4, 7])]
+
+# TODO: create dynamically
+BASE_HARMONIES = {2:[set([6]), set([6, 2]), set([6, 0]), set([0, 2]), set([0, 4])],
+                  3:[set([0]), set([6]), set([6, 2]), set([6, 0]), set([0, 4]), set([0, 2]), set([0, 2, 4]), set([2, 4])],
+                  4:[set([0, 2]), set([6]), set([6, 2]), set([6, 0]), set([0, 4]), set([0, 2]), set([0, 2, 4]), set([2, 4])]
+                  }
 HARMONIES = STRICT_HARMONIES + [set([2, 4, 1]), set([2, 6, 1])]
 HARMONIC_INTERVALS = [0, 2, 3, 4, 5, 6]
 
