c7559bf1966fee2157cbf253baa6c085b7e05a83
Christian Wörner <christianworner@gmail.com>
2011-05-15 19:21:03 +0200

    Note-Gateway: apply message sending blocking, plus:
    - some configs
    - some docstrings

--- roqba/note_gateway.py
+++ roqba/note_gateway.py
@@ -1,41 +1,38 @@
 import logging
 import time
+
 from metronome import HEAVY
-
 from pdsender import PdSender
-
-#HOST = "localhost"
-#PORT = 9999
-PD_HOST = "localhost"
-PD_PORT = 12321
-block_messages = False
-TRANSPOSE = 12
 
 
 class NoteGateway(object):
     def __init__(self, settings, behaviour):
-        ## XxxxX: make these settings configurable
         self.logger = logging.getLogger("sender")
         self.settings = settings
         self.slide = True
-        self.slide_duration_prop = 0.7
+        self.slide_duration_prop = behaviour['default_slide_duration_prop']
         self.voice_ids = []
+        self.block_messages = False
         self.transpose = behaviour["transpose"]
         self.pd = PdSender(settings["PD_HOST"], settings["PD_PORT"])
 
     def pause(self):
+        '''blocks new messages and turns off sound-production'''
         self.block_messages = True
         self.pd.send("sound 0")
 
     def unpause(self):
+        '''turns on sound-production and lets messages flow again'''
         self.block_messages = True
         self.pd.send("sound 1")
 
     def stop(self):
+        '''destroys the puredata-sender object'''
         time.sleep(1)
         del self.pd
 
     def stop_all_notes(self):
+        '''sends a stop message to all active voices'''
         for v in self.voice_ids:
             self.pd.send(["voice", v, 0])
 
@@ -49,6 +46,11 @@ class NoteGateway(object):
     def set_slide_msecs(self, voice_id, msecs):
         '''sends a message containing the general slide-time in millisecs'''
         self.pd.send(["voice", "slide", voice_id, msecs])
+
+    def set_slide_msecs_for_all_voices(self, msecs):
+        '''sends a message containing the general slide-time in millisecs'''
+        for v in self.voice_ids:
+            self.pd.send(["voice", "slide", v, msecs])
 
     def pd_send_note(self, voice_id, msg):
         '''send out a single note'''
@@ -69,16 +71,17 @@ class NoteGateway(object):
         according to present state'''
         while True:
             data = (yield)
-            self.logger.info("drums out: {0}".format(data))
-            for k, v in data.items():
-                if v["meta"] != "empty":
-                    args = ["perc", k]
-                    if v["vol"]:
-                        args.append(v["vol"])
-                    args.append(v["pan"])
-                    if v["ctl"]:
-                        args.append(v["ctl"])
-                    self.pd.send(args)
+            if not self.block_messages:
+                self.logger.info("drums out: {0}".format(data))
+                for k, v in data.items():
+                    if v["meta"] != "empty":
+                        args = ["perc", k]
+                        if v["vol"]:
+                            args.append(v["vol"])
+                        args.append(v["pan"])
+                        if v["ctl"]:
+                            args.append(v["ctl"])
+                        self.pd.send(args)
 
     def hub(self):
         '''generator method that sends notes to all melodic voices
@@ -86,25 +89,27 @@ class NoteGateway(object):
         according to the present state'''
         while True:
             data = (yield)
-            self.logger.info("sending out: {0}".format(data))
-            if type(data) == dict:
-                for v in data.values():
-                    if v.note_change:
-                        msg = v.real_note if v.real_note else 0
-                        #self.logger.info("sending out: voice: {1}: {0}".\
-                        #                  format(msg, v.id))
-                        if self.slide and v.slide:
-                            if v.slide_duration_prop:
-                                dur_prop = v.slide_duration_prop
-                            else:
-                                dur_prop = self.slide_duration_prop
-                            self.set_slide_msecs(v.id,
-                                            (v.duration_in_msec * dur_prop))
-                        self.pd_send_note(v.id, msg)
-                        if v.weight == HEAVY:
-                            self.pd.send(["voice", "rhythm", v.id,
-                                str(v.note_length_grouping).replace(",", "_")])
-                #address = data["voice"]
-                #msg = data["message"]
-            else:
-                msg = str(data)
+            if not self.block_messages:
+                self.logger.info("sending out: {0}".format(data))
+                if type(data) == dict:
+                    for v in data.values():
+                        if v.note_change:
+                            msg = v.real_note if v.real_note else 0
+                            #self.logger.info("sending out:\
+                            #                  voice: {1}: {0}".\
+                            #                  format(msg, v.id))
+                            if self.slide and v.slide:
+                                if v.slide_duration_prop:
+                                    dur_prop = v.slide_duration_prop
+                                else:
+                                    dur_prop = self.slide_duration_prop
+                                self.set_slide_msecs(v.id,
+                                                (v.duration_in_msec * dur_prop))
+                            self.pd_send_note(v.id, msg)
+                            if v.weight == HEAVY:
+                                self.pd.send(["voice", "rhythm", v.id,
+                                    str(v.note_length_grouping).replace(",", "_")])
+                    #address = data["voice"]
+                    #msg = data["message"]
+                else:
+                    msg = str(data)
