071636fede4b7235c8aef3af8ab42ea2b5422c4d
Christian Wörner <christianworner@gmail.com>
2011-08-17 19:19:35 +0200

    GUI: more widgets, debug and better syncronization

--- roqba/ui/main.py
+++ roqba/ui/main.py
@@ -1,9 +1,10 @@
 import socket
 import json
+from collections import OrderedDict
 from math import floor
 from Tkinter import tkinter
-from Tkinter import Frame, HORIZONTAL, IntVar, W, N, E
-from Tkinter import Checkbutton, Scale, Button
+from Tkinter import Frame, HORIZONTAL, IntVar, StringVar, W, N, E
+from Tkinter import LabelFrame, Checkbutton, Radiobutton, Scale, Button
 
 # TODO?: integration into roqba
 #from roqba.static.settings import settings
@@ -13,27 +14,31 @@ port = 12322
 send_port = 12323
 
 
-CHECK_BUTTONS = {'automate_speed_change': {'val': True, 'disable': True},
-                 'automate_shuffle': True,
-                 'automate_meters': True,
-                 'automate_wavetables': True,
-                 'automate_num_partials': True,
-                 'automate_transpose': True,
-                 'automate_slide': {'val': True, 'disable': True},
-                 'automate_pan': True,
-                 'automate_binaural_diffs': True,
-                 'pan_controls_binaural_diff': True,
-                 'common_wavetables': True,
-                 'play': True}
+CHECK_BUTTONS = OrderedDict([
+                 ('automate_speed_change', {'val': True, 'disable': True}),
+                 ('automate_shuffle', True),
+                 ('automate_meters', True),
+                 ('automate_wavetables', True),
+                 ('automate_num_partials', False),
+                 ('automate_transpose', True),
+                 ('automate_slide', {'val': True, 'disable': True}),
+                 ('automate_pan', True),
+                 ('automate_binaural_diffs', True),
+                 ('pan_controls_binaural_diff', True),
+                 ('common_wavetables', True),
+                 ('play', {'val': True, 'sub_frame': 'monitor_frame'})
+               ])
 
-SCALES = {'caesura_probability': {'min': 0.01, 'max': 1, 'start': 0.666, 'res': 0.001},
-          'slide_in_msec': {'min': 0, 'max': 5000, 'start': 200, 'res': 1, 'enable':'automate_slide'},
+SCALES = {'caesura_prob': {'min': 0.01, 'max': 1, 'start': 0.666, 'res': 0.001, 'label': 'probability of a caesura'},
+          'slide_duration_msecs': {'min': 0, 'max': 5000, 'start': 200, 'res': 1, 'enable':'automate_slide', 'label': 'slide duration in msecs'},
           'transpose': {'min': -24, 'max': 24, 'start': 12, 'res': 1},
           'shuffle_delay': {'min': 0, 'max': 1, 'start': 0.2, 'res': 0.0001},
           'speed': {'min': 0.1, 'max': 1.2, 'start': 0.2, 'res': 0.0001, 'enable': 'automate_speed_change'},
           'max_shuffle': {'min': 0, 'max': 0.8, 'start': 0.2, 'res': 0.0001},
           'binaural_diff': {'min': 0, 'max': 66, 'start': 0.2, 'res': 0.01},
           'max_binaural_diff': {'min': 0, 'max': 66, 'start': 30, 'res': 0.01},
+          'speed_target': {'min': 0,'max': 1, 'start': 0.5, 'res': 0.001,
+                           'label': 'distort speed (slower/faster)', 'pos': {'c':2,'r':2}}
          }
 
 RANGES = {'speed': {'min': 0.1, 'max': 1.2, 'min_start': 0.2, 'max_start': 1.1, 'res': 0.001, 'disable': 'automate_speed_change'}}
@@ -52,61 +57,128 @@ class Application(Frame):
         self.columnconfigure(4, minsize=150)
         self.columnconfigure(5, minsize=150)
         self.columnconfigure(6, minsize=150)
-        self.createWidgets()
+        self.create_widgets()
+        self.settables = self.assemble_settables()
+        self.request_update()
 
-    def createWidgets(self):
+    def create_widgets(self):
+        self.create_monitor()
         self.create_check_buttons()
         self.create_ranges()
         self.create_scales()
+        self.create_radio_buttons()
         self.create_voices()
-        self.create_monitor()
         self.quitButton = Button(self, text='Quit', command=self.quit)
         self.quitButton.grid(columnspan=7, sticky=E+W)
 
+    def assemble_settables(self):
+        settables = self.winfo_children()
+        for w in settables:
+            settables += w.winfo_children()
+        return filter(lambda w: w.__class__.__name__ in ['Scale', 'Checkbutton'], settables)
+
+    def create_radio_buttons(self):
+        entries =  ['DIATONIC', 'HARMONIC', 'MELODIC', 'PENTATONIC', 'PENTA_MINOR']
+        self.scale = StringVar()
+        self.scale.set('DIATONIC')
+        print self.scale, self.scale.get()
+        self.rb_frame = Frame(self)
+        for e in entries:
+            rb = Radiobutton(self.rb_frame, value=e, text=e, anchor=W, command=self.send_scale, variable=self.scale)
+            rb.grid(row=len(self.rb_frame.winfo_children()), sticky=W)
+        self.rb_frame.grid(column=1, row=len(self.grid_slaves(column=1)),rowspan=3)
+
     def create_monitor(self):
-        this= Scale(self, label='cycle_pos', orient=HORIZONTAL,
+        self.monitor_frame = LabelFrame(self, text="Monitor and Transport")
+        this_cycle = Scale(self.monitor_frame, label='cycle_pos', orient=HORIZONTAL,
                          from_=1, to=16, resolution=1)
-        this.disable, this.enable = (None, None)
-        this.ref = 'cycle_pos'
-        this.grid(column=0, row=10,sticky=E+W)
+        this_cycle.disable, this_cycle.enable = (None, None)
+        this_cycle.ref = 'cycle_pos'
+        this_cycle.grid(column=0, row=0, sticky=E+W)
+        self.updateButton = Button(self.monitor_frame, text='Reload all Settings', command=self.request_update)
+        self.updateButton.grid(row=1, sticky=E+W)
+        self.monitor_frame.grid(column=0, row=10, sticky=E+W)
+
+    def request_update(self):
+        self.send({'sys': 'update'})
 
     def create_voices(self):
         voice_ids = ['1','2','3','4']  
-        elements = {'pan_pos': {'min': -1, 'max': 1, 'start': 0.5, 'res': 0.001},
-                    'binaural_diff': {'min': 0, 'max': 66, 'start': 0.2, 'res': 0.01}}
+        SCALES = OrderedDict([
+                  ('pan_pos', {'min': -1, 'max': 1, 'start': 0.5, 'res': 0.001}),
+                  ('slide_duration_msecs', {'min': 0, 'max': 2000, 'start': 60, 'res': 1}),
+                  ('slide_duration_prop', {'min': 0, 'max': 2, 'start': 0.666, 'res': 0.001}),
+                  ('binaural_diff', {'min': 0, 'max': 66, 'start': 0.2, 'res': 0.01})
+                ])
+        
         for vid in voice_ids:
             counter = 0
-            for ele in elements:
-                name = 'voice_' + vid + '_' + ele
-                setattr(self, 'min_' + name, elements[ele]['min'])
-                setattr(self, 'max_' + name, elements[ele]['max'])
-                this_ele = Scale(self, label=name, orient=HORIZONTAL,
+            for sca in SCALES:
+                name = 'voice_' + vid + '_' + sca
+                setattr(self, 'min_' + name, SCALES[sca]['min'])
+                setattr(self, 'max_' + name, SCALES[sca]['max'])
+                this_sca = Scale(self, label=sca, orient=HORIZONTAL,
                                         from_=getattr(self, 'min_' + name), to=getattr(self, 'max_' + name), 
-                                        resolution=elements[ele]['res'])
-                this_ele.enable = ('enable' in elements[ele].keys() and elements[ele]['enable'] or None)
-                this_ele.disable = ('disable' in elements[ele].keys() and elements[ele]['disable'] or None)
-                this_ele.grid(column=int(2 + int(vid)), row=counter, sticky=E+W)
-                this_ele.ref = name
+                                        resolution=SCALES[sca]['res'])
+                this_sca.enable = ('enable' in SCALES[sca].keys() and SCALES[sca]['enable'] or None)
+                this_sca.disable = ('disable' in SCALES[sca].keys() and SCALES[sca]['disable'] or None)
+                this_sca.grid(column=int(2 + int(vid)), row=counter, sticky=E+W)
+                this_sca.bind("<ButtonRelease>", self.scale_handler)
+                this_sca.ref = name
                 counter += 1
+        CHECK_BUTTONS = OrderedDict(
+                 [('automate_binaural_diffs', True),
+                  ('automate_note_duration_prop', True),
+                  ('use_proportional_slide_duration', {'val': True, 'label': 'proportional slide'}),
+                  ('automate_pan', True),
+                  ('automate_wavetables', True)])
+        for vid in voice_ids:
+            counter = 0
+            cb_frame = LabelFrame(self, text="Voice {0} - Automation".format(vid))
+            for cb in CHECK_BUTTONS:
+                options = CHECK_BUTTONS[cb]
+                name = 'voice_' + vid + '_' + cb 
+                label = (options['label'] if isinstance(options, dict) and 'label' in options.keys() else
+                                              (cb[9:] if cb[:9] == 'automate_' else cb))
+                setattr(self, name, IntVar(value=type(options) == dict and options['val'] or options))
+                self.this_cb = Checkbutton(cb_frame, text=label, variable=getattr(self, name))
+                self.this_cb.bind('<Button-1>', self.check_boxes_handler)
+                self.this_cb.disable = None
+                self.this_cb.grid(sticky=W, column=0, row=counter)
+                self.this_cb.ref = name
+                counter += 1
+            cb_frame.grid(column=int(vid) + 2, row=4, sticky=N, rowspan=2)
+
 
     def create_check_buttons(self):
-        counter = 0
-        self.cb_frame = Frame(self)
+        self.cb_frame = LabelFrame(self, text="Global Settings")
         for cb in CHECK_BUTTONS:
+            label = cb 
+            target_parent = self.cb_frame
+            if isinstance(CHECK_BUTTONS[cb], dict) and  'sub_frame' in CHECK_BUTTONS[cb].keys():
+                target_parent = getattr(self, CHECK_BUTTONS[cb]['sub_frame'])
             setattr(self, cb, IntVar(value=type(CHECK_BUTTONS[cb]) == dict and CHECK_BUTTONS[cb]['val'] or CHECK_BUTTONS[cb]))
-            self.this_cb = Checkbutton(self.cb_frame, text=cb, variable=getattr(self, cb))
+            self.this_cb = Checkbutton(target_parent, text=label, variable=getattr(self, cb))
             self.this_cb.bind('<Button-1>', self.check_boxes_handler)
             self.this_cb.disable = type(CHECK_BUTTONS[cb]) == dict and 'disable' in CHECK_BUTTONS[cb].keys()
-            self.this_cb.grid(sticky=W, column=0, row=counter)
+            self.this_cb.grid(sticky=W, column=0, row=len(target_parent.winfo_children()))
             self.this_cb.ref = cb
-            counter += 1
-        self.cb_frame.grid(column=0, rowspan=10, sticky=N)
+        self.cb_frame.grid(column=0, row=0, rowspan=10, sticky=N)
 
     def set_value(self, name, val):
-        '''set a scale to the specified value'''
-        for w in self.children.values():
-            if w.__class__.__name__ == 'Scale' and w.ref == name:
-                w.set(val)
+        '''set a widget to the specified value'''
+        if name == 'scale':
+            self.scale.set(val)
+            return
+        for w in self.settables:
+            typ = w.__class__.__name__
+            if w.ref == name:
+                #print "setting '{0}' of type: '{1}' to: {2}".format(name, typ, val)
+                if typ == 'Scale':
+                    w.set(val)
+                elif typ == "Checkbutton":
+                    w.select() if val else w.deselect()
+
         
     def check_boxes_handler(self, event):
         '''handles checkbox events.
@@ -119,51 +191,67 @@ class Application(Frame):
         self.send({ref: val})
         print ref, val 
         # handle gui elements
-        if event.widget.disable:
-            for w in self.children.values():
-                # this try clause is for debugging, remove when stable 
-                try:
-                    w.ref
-                    #print w.ref
-                except:
-                    pass
-                if (w.__class__.__name__ == 'Scale' and 
-                    (w.disable or w.enable)):
-                    if w.disable == ref:
-                        if val:
-                            w.grid_remove()
-                        else:
-                            w.grid()
-                    elif w.enable == ref:
-                        if val:
-                            w.grid()
-                        else:
-                            w.grid_remove()
-                    #print w.disable, w.enable
+        # enable/disable functionality temporarily(?) commented on: 
+        # Wed Aug 17 09:39:54 CEST 2011
+#        if event.widget.disable:
+#            for w in self.children.values():
+#                
+#                # this try clause is for debugging, remove when stable 
+#                try:
+#                    w.ref
+#                    #print w.ref
+#                except:
+#                    pass
+#                if (w.__class__.__name__ == 'Scale' and 
+#                    (w.disable or w.enable)):
+#                    if w.disable == ref:
+#                        if val:
+#                            w.grid()
+#                        else:
+#                            w.grid_remove()
+#                    elif w.enable == ref:
+#                        if val:
+#                            w.grid_remove()
+#                        else:
+#                            w.grid()
+#                    #print w.disable, w.enable
 
     def create_scales(self): 
         counter = 0
         for sca in SCALES:
+            label = SCALES[sca]['label'] if 'label'  in SCALES[sca].keys() else sca
             setattr(self, 'min_' + sca, SCALES[sca]['min'])
             setattr(self, 'max_' + sca, SCALES[sca]['max'])
-            self.this_scale = Scale(self, label=sca, orient=HORIZONTAL,
+            self.this_scale = Scale(self, label=label, orient=HORIZONTAL,
                                     from_=getattr(self, 'min_' + sca), to=getattr(self, 'max_' + sca), 
                                     resolution=SCALES[sca]['res'])
             self.this_scale.set(SCALES[sca]['start'])
             self.this_scale.enable = ('enable' in SCALES[sca].keys() and SCALES[sca]['enable'] or None)
             self.this_scale.disable = ('disable' in SCALES[sca].keys() and SCALES[sca]['disable'] or None)
-            self.this_scale.grid(column=1, row=counter, sticky=E+W)
+            if 'pos' in SCALES[sca].keys():
+                pos =  SCALES[sca]['pos']
+                col = pos['c']
+                row = pos['r']
+            else:
+                row = counter
+                col = 1
+                counter += 1
+            self.this_scale.grid(column=col, row=row, sticky=E+W)
             self.this_scale.ref = sca
             self.this_scale.bind("<ButtonRelease>", self.scale_handler)
-            counter += 1
 
     def scale_handler(self, event):
         self.send({event.widget.ref: event.widget.get()})
         print event.widget.ref, event.widget.get()
 
+    def send_scale(self):
+        do = {'scale':self.scale.get()}
+        self.send(do)
+        print do
+
     def send(self, msg):
+        print "sending: ", msg
         self.send_sock.sendto(json.dumps(msg), (host, send_port))
-        pass
 
     def create_ranges(self): 
         counter = 0
@@ -186,11 +274,14 @@ class Application(Frame):
             self.this_max_scale.grid(column=2, row=counter + 1, sticky=E+W)
             self.this_min_scale.ref = 'min_' + ran
             self.this_max_scale.ref = 'max_' + ran
+            self.this_min_scale.bind("<ButtonRelease>", self.scale_handler)
+            self.this_max_scale.bind("<ButtonRelease>", self.scale_handler)
             counter += 2
 
     def socket_read_handler(self, file, mask):
         data_object = json.loads(file.recv(1024))
         do = data_object.items()[0]
+        #print do
         self.set_value(do[0], do[1])
 
 app = Application()
