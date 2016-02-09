12e8c5aa6b1f8635b3851e2b6f518a312ef6e773
Christian Wörner <christianworner@gmail.com>
2016-02-10 00:15:57 +0100

    baroq: user rhythm and meter mixin

--- roqba/composers/baroq.py
+++ roqba/composers/baroq.py
@@ -12,9 +12,10 @@ from roqba.static.scales_and_harmonies import (ALL_STRICT_HARMONIES,
 from roqba.static.meters import METERS
 from roqba import metronome
 from roqba.composers.abstract_composer import AbstractComposer
+from roqba.composers.rhythm_and_meter_mixin import RhythmAndMeterMixin
 
 
-class Composer(AbstractComposer):
+class Composer(RhythmAndMeterMixin, AbstractComposer):
     def __init__(self,
                  gateway,
                  settings,
@@ -108,11 +109,6 @@ class Composer(AbstractComposer):
                         v.real_note += 1
                     self.musical_logger.info("modified note: {0} from '{1}' to '{2}'".format(
                                              v.note, was_note, v.real_note))
-#        for v in self.voices.values():
-#            if v.note_change:
-#                v.real_note = self.scale_walker(self.scale,
-#                                                v.real_note,
-#                                                v.note_delta)
 
     def acceptable_harm_for_length(self, harm, length):
         '''checks if the specified (interval-set) are "harmonic"'''
@@ -152,13 +148,6 @@ class Composer(AbstractComposer):
             voices = [melodic[0]] + voices
         return voices
 
-    def choose_rhythm(self):
-        '''chooses a new rhythm randomly from each voices groupings'''
-        for v in self.voices.values():
-            if not v.playing_a_melody:
-                v.set_rhythm_grouping(
-                    choice([grouping for grouping in v.note_length_groupings
-                            if sum(grouping) == self.meter[0]]))
 
     def embellish(self, state):
         '''checks for embellishment markers of the single voices
@@ -173,35 +162,6 @@ class Composer(AbstractComposer):
                                        v.note,
                                        v.note_delta,
                                        state)).start()
-
-    def drum_fill_handler(self, v, state):
-        '''handles the sending of drum-fill notes'''
-        identifier = 'cont' if v == 'cont2' else 'cont2'
-        possible_fills = [fill for fill in DRUM_FILLS
-                          if state['speed'] / len(fill) > self.behaviour['min_speed'] * 0.5]
-        chosen = choice(possible_fills)
-        for fraction in chosen:
-            if (state["speed"] * 1000 * fraction) < self.drummer.peak_speed:
-                break
-            self.gateway.pd_send_drum_note(identifier, self.drummer.frame[identifier]["vol"],
-                                           self.drummer.frame[identifier]["pan"],
-                                           self.drummer.frame[identifier]["ctl"])
-            time.sleep(state["speed"] * fraction)
-
-    def drum_mark_handler(self, v, state):
-        '''handles the sending of a drum mark'''
-        vol = self.drummer.frame[v]["vol"],
-        pan = self.drummer.frame[v]["pan"],
-        ctl = self.drummer.frame[v]["ctl"],
-        tick = state["speed"] / 50
-        vol_tmp = vol[0]
-        for idx in xrange(6):
-            vol_tmp *= 0.9
-            ctl_new = ctl[0] - ((idx ** 2) * (random.random() * 600 + 200))
-            self.gateway.pd_send_drum_note(v, vol_tmp, pan[0], ctl_new)
-            time.sleep(4 * tick)
-            self.gateway.pd_send_drum_note(v, 0, pan[0], ctl_new)
-            time.sleep(1 * tick)
 
     def ornament_handler(self, v, duration, note, note_delta, state):
         '''this method handles the sending of the ornament notes.
