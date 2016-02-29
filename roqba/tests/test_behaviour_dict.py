0eabf4d2aa64aebbe812766702fcdc25863aee84
Christian Wörner <christianworner@gmail.com>
2016-02-29 16:09:20 +0100

    tests: port behaviour dict

--- roqba/tests/test_behaviour_dict.py
+++ roqba/tests/test_behaviour_dict.py
@@ -1,44 +1,46 @@
 """unit tests for the BehaviourDict
 
 """
-import unittest2 as unittest
+import pytest
+
 
 from mock import Mock
-from roqba.utilities import behaviour_dict
+from roqba.utilities.behaviour_dict import BehaviourDict
 
 
-class UnitTestBehaviourDict(unittest.TestCase):
-    """a test class for the BehaviourDict class"""
-    def setUp(self):
-        """
-        set up data used and mock objects/methods in the tests.
-        """
-        self.bd = behaviour_dict.BehaviourDict({11:2, 13:4, 
-                                                'per_voice':{1: {11:3}
-                                               }})
-
-    def test_voice_get_should_return_voice_behaviour_if_set(self):
-        '''should return a set behaviour for a voice'''
-        self.assertEqual(self.bd.voice_get(1, 11), 3)
-   
-    def test_voice_get_should_return_default_behaviour_if_not_set_for_voice(self):
-        '''should return default if no behaviour for a voice'''
-        self.assertEqual(self.bd.voice_get(1, 13), 4)
-   
-    def test_voice_get_should_raise_runtime_error_for_no_voice(self):
-        '''should raise RuntimeError if called for non-existant voice'''
-        self.assertRaises(RuntimeError, self.bd.voice_get, 2,'any')
-
-    def test_voice_get_should_raise_runtime_error_for_wrong_key(self):
-        '''should raise RuntimeError for wrong key'''
-        self.assertRaises(RuntimeError, self.bd.voice_get, 1,'any')
-
-def suite():
-    """make the test suite"""
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(UnitTestBehaviourDict))
-    return suite
+@pytest.fixture()
+def behaviour_dict(request):
+    """
+    set up data used and mock objects/methods in the tests.
+    """
+    behaviour_dict = BehaviourDict({
+        11: 2, 13: 4,
+        'per_voice': {
+            1: {
+                11: 3
+            }
+        }
+    })
+    return behaviour_dict
 
 
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
+def test_voice_get_should_return_voice_behaviour_if_set(behaviour_dict):
+    '''should return a set behaviour for a voice'''
+    assert behaviour_dict.voice_get(1, 11) == 3
+
+
+def test_voice_get_should_return_default_behaviour_if_not_set_for_voice(behaviour_dict):
+    '''should return default if no behaviour for a voice'''
+    assert behaviour_dict.voice_get(1, 13) == 4
+
+
+def test_voice_get_should_raise_runtime_error_for_no_voice(behaviour_dict):
+    '''should raise RuntimeError if called for non-existant voice'''
+    with pytest.raises(RuntimeError):
+        behaviour_dict.voice_get(2,'any')
+
+
+def test_voice_get_should_raise_runtime_error_for_wrong_key(behaviour_dict):
+    '''should raise RuntimeError for wrong key'''
+    with pytest.raises(RuntimeError):
+        behaviour_dict.voice_get(1,'any')
