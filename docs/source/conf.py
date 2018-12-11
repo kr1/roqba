752ce7f6e0cae8a61d2d30eff7c8f3ef0f1438e7
Christian Wörner <christianworner@gmail.com>
2018-12-11 21:20:28 +0000

    Docs: change theme to sphinxdoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -98,6 +98,10 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'alabaster'  #'nature'  # 'default'
+html_theme = 'classic'  #'nature'  # 'default'
+html_theme = 'traditional'  #'nature'  # 'default'
+html_theme = 'haiku'  #'nature'  # 'default'
+html_theme = 'sphinxdoc'  #'nature'  # 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
