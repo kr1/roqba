da5b957f75fde54bdb5d832163e64b87e529ccf9
Christian Wörner <christianworner@gmail.com>
2018-12-29 09:58:08 +0100

    Tests: start tests for static usualis module

---
+++ roqba/tests/static/test_usualis.py
@@ -0,0 +1,16 @@
+import pytest
+from roqba.static.usualis import next_valid_word, UsualisError
+
+higher = [-1, 0, 1, 2, 8, 9, 10, 11, 12]
+lower = [1, 0, -1, -2, -8, -9, -10, -11, -12]
+
+
+@pytest.mark.parametrize('higher', higher)
+@pytest.mark.parametrize('lower', lower)
+@pytest.mark.parametrize('start_note', higher + lower)
+def test_next_valid_word(start_note, lower, higher):
+    if higher < 0 or lower > 0:
+        with pytest.raises(UsualisError):
+            assert next_valid_word(start_note, higher, lower)
+    else:
+        assert next_valid_word(start_note, higher, lower)
