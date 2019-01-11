f10d2b447ea30fe7a15844aa246f5bdedfb8eac9
Christian Wörner <christianworner@gmail.com>
2019-01-11 23:01:29 +0100

    Usualis: adapt a usualis test to latest changes

--- roqba/tests/static/test_usualis.py
+++ roqba/tests/static/test_usualis.py
@@ -9,7 +9,7 @@ lower = [1, 0, -1, -2, -8, -9, -10, -11, -12]
 @pytest.mark.parametrize('lower', lower)
 @pytest.mark.parametrize('start_note', higher + lower)
 def test_next_valid_word(start_note, lower, higher):
-    if higher < 0 or lower > 0:
+    if higher < 0 or lower > 0 or (lower == higher == 0):
         with pytest.raises(UsualisError):
             assert next_valid_word(start_note, higher, lower)
     else:
